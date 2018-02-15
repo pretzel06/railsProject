0ac5030175c1c5f7b7978836fc6f13fa0a2920bd
lester <lester.lamsen@gmail.com>
2018-02-15 03:25:13 -0600

    a completed run of seed files
    successfully added customer product store and transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#add json helper
+require 'json'
+require 'pp'
+
+#clear the data in the table for now
+Customer.delete_all
+Store.delete_all
+Product.delete_all
+
+5.times do
+  Customer.create(customer_name: Faker::GameOfThrones.character,
+                  customer_address: Faker::Address.state)
+end
+puts "my customer count: #{Customer.count}"
+
+#read json file from path rails.root
+data = JSON.parse(open("#{Rails.root}/data.json").read)
+
+data.each do |store|
+  Store.create(store_name: store['store'])
+
+  store_id = Store.last.id
+
+  store['result'].each do |product|
+    Product.create(product_name: product['item_name'],
+                   product_quantity: Faker::Number.number(2),
+                   product_price: product['item_sale_price'],
+                   store_id: store_id)
+  end
+end
+
+puts "my store count: #{Store.count}"
+puts "my product count: #{Product.count}"
+
+pp Customer.all
+pp Product.all
+
+customers = Customer.all
+products = Product.all
+
+customers.each do |customer|
+  new_transaction = Transaction.new
+  new_transaction.customer_id = customer.id
+  new_transaction.name = customer.customer_name
+  quantity = Faker::Number.between(1, 9)
+  new_transaction.quantity = quantity
+  random_product = Faker::Number.between(0, Product.count - 1)
+  new_transaction.total_cost = quantity * products[random_product].product_price
+  new_transaction.product_id = products[random_product].id
+
+  new_transaction.save
+end
