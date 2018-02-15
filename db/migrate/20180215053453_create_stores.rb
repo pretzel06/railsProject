7f92345c2e0ff2ee7e359557e4fad8b15d12d934
lester <lester.lamsen@gmail.com>
2018-02-15 11:04:04 -0600

    I did a proper migration as Kyle told me
    VERSION=0 rollback to early version and did a db:migrate

--- db/migrate/20180215053453_create_stores.rb
+++ db/migrate/20180215053453_create_stores.rb
@@ -1,6 +1,5 @@
 class CreateStores < ActiveRecord::Migration[5.1]
   def change
-    drop_table :stores
     create_table :stores do |t|
       t.string :store_name
 
--- db/migrate/20180215053507_create_transactions.rb
+++ db/migrate/20180215053507_create_transactions.rb
@@ -1,6 +1,5 @@
 class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
-    drop_table :transactions
     create_table :transactions do |t|
       t.string :name
       t.integer :quantity
